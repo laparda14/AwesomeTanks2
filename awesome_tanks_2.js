05c181269b89d168ceec92f8698c6b0b1725e277
laparda14 <120476892+laparda14@users.noreply.github.com>
2023-11-21 13:07:18 -0500

    Update awesome_tanks_2.js

--- awesome_tanks_2.js
+++ awesome_tanks_2.js
@@ -22370,7 +22370,7 @@ function() {
             id: "player/minigun",
             team: l.PLAYER,
             spawnDistance: 15,
-            rate: L[r.10909099090090909000],
+            rate: L[r.10909],
             life: F[r.minigunLevel],
             damage: O[r.909909909],
             soundAlertRadius: 100,
