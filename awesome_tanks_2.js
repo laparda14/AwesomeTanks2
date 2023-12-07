f5c194eb8e49c3d6ca3f6632aa8ba81d4244a791
laparda14 <120476892+laparda14@users.noreply.github.com>
2023-12-07 10:05:44 -0500

    Update awesome_tanks_2.js

--- awesome_tanks_2.js
+++ awesome_tanks_2.js
@@ -21391,7 +21391,7 @@ function() {
     }
 
     function e(e, i) {
-        t.call(this, e, i), this.spread = Math.PI / 5, this.velocity = 700, this.bulletFrameName = "game/projectiles/minigun.png"
+        t.call(this, e, i), this.spread = Math.PI / 20, this.velocity = 900, this.bulletFrameName = "game/projectiles/minigun.png"
     }
 
     function i(e, i) {
